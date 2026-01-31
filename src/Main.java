ae06e84b836d11a85cf0e727814c5af2e582352a
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-31 23:05:46 +0530

    made whether predicting app using java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter city name: ");
+        String city = sc.nextLine().toLowerCase();
+
+        int temperature = 0;
+        String condition = "";
+        int humidity = 0;
+
+        if (city.equals("pune")) {
+            temperature = 30;
+            condition = "Sunny";
+            humidity = 40;
+        }
+        else if (city.equals("mumbai")) {
+            temperature = 28;
+            condition = "Humid";
+            humidity = 70;
+        }
+        else if (city.equals("delhi")) {
+            temperature = 35;
+            condition = "Hot";
+            humidity = 30;
+        }
+        else if (city.equals("bangalore")) {
+            temperature = 25;
+            condition = "Cloudy";
+            humidity = 50;
+        } else if (city.equals("kolhapur")) {
+            temperature = 20;
+            condition = "Clear";
+            humidity = 30;
+
+        } else if (city.equals("gao")) {
+            temperature = 28;
+            condition = "Clear";
+            humidity = 70;
+
+        } else {
+            System.out.println("\nCity not found in database.");
+            sc.close();
+            return;
+        }
+
+        System.out.println("\n🌦 Weather Report");
+        System.out.println("City: " + city);
+        System.out.println("Temperature: " + temperature + "°C");
+        System.out.println("Condition: " + condition);
+        System.out.println("Humidity: " + humidity + "%");
+
+        sc.close();
+    }
 }
