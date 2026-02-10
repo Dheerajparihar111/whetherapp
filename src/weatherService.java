9e873356a221f70aa91334ed9c24ddddb82310bd
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-02-10 21:04:27 +0530

    handles api calling

---
+++ src/weatherService.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherService {
+
+    public String fetchWeather(String city) {
+
+        try {
+            String apiUrl = WeatherConfig.BASE_URL
+                    + city
+                    + "?key=" + WeatherConfig.API_KEY
+                    + "&unitGroup=metric";
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream())
+            );
+
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+
+            br.close();
+            return response.toString();
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
